4b87461ca3ad839306fd95391f76c45e3fcee1b3
nghinv <nghiweb@gmail.com>
2024-05-06 14:11:39 +0700

    feat: ts-validator-schemagit status

---
+++ README.md
@@ -0,0 +1,88 @@
+# @nixjs23n6/ts-validator-schema
+
+Typescript - create message error keys and parameters in Zod schema and flattening message keys.
+
+## Install
+
+```shell [npm]
+npm install @nixjs23n6/ts-validator-schema
+```
+
+```shell [pnpm]
+pnpm add @nixjs23n6/ts-validator-schema
+```
+
+```shell [yarn]
+yarn add @nixjs23n6/ts-validator-schema
+```
+
+```shell [bun]
+bun add --dev husky
+```
+
+## Example
+
+```typescript
+import { BaseMessage, Define, flattenSchemaValue } from '@athena20/ts-validator-schema'
+import * as z from 'zod'
+
+namespace ValidatorConfig {
+    export const BaseMessageErrorPrefix = 'msg_error'
+    export type Key = 'username' | 'email' | 'password' | 'confirmPassword' | 'oldPassword'
+    export type Message = BaseMessage | 'passwords_cannot_be_the_same' | 'passwords_must_be_match' | 'email_regex_invalid' | 'email_existed'
+    export const Schema: Define<Key, Message> = {
+        username: {
+            required: { message: 'default_required' },
+            min: { message: 'required_min', value: 8 },
+            max: { message: 'required_max', value: 50 },
+            regex: {
+                message: 'regex_invalid',
+                value: /^[a-zA-Z0-9]*$/
+            }
+        },
+        oldPassword: { required: { message: 'default_required' }, min: { message: 'required_min', value: 6 } },
+        password: {
+            required: { message: 'default_required' },
+            min: { message: 'required_min', value: 6 },
+            max: { message: 'required_max', value: 20 },
+            regex: { message: 'regex_invalid', value: /^[a-zA-Z0-9`!@#$%^&*()_+\-=\\[\]{};:"|,.<>?~']+$/ },
+            extra: {
+                message: 'passwords_cannot_be_the_same'
+            }
+        },
+        confirmPassword: {
+            required: { message: 'default_required' },
+            min: { message: 'required_min', value: 6 },
+            max: { message: 'required_max', value: 20 },
+            extra: {
+                message: 'passwords_must_be_match'
+            }
+        },
+        email: {
+            required: { message: 'default_required' },
+            min: { message: 'required_min', value: 5 },
+            max: { message: 'required_max', value: 255 },
+            email: { message: 'email_invalid' },
+            extra: { message: 'email_existed' },
+            regex: { message: 'email_regex_invalid', value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i }
+        }
+    }
+    export const FlattenSchema = flattenSchemaValue(Schema, BaseMessageErrorPrefix) // console.log(FlattenSchema)
+}
+
+export const zEmailRequiredValidator = z
+    .string()
+    .email(ValidatorConfig.Schema.email.email?.message)
+    .min(ValidatorConfig.Schema.email.min?.value!, ValidatorConfig.Schema.email.min?.message)
+    .max(ValidatorConfig.Schema.email.max?.value!, ValidatorConfig.Schema.email.max?.message)
+
+export const zPasswordValidator = zRequiredValidator(ValidatorConfig.Schema.password.required?.message)
+    .min(ValidatorConfig.Schema.password.min?.value!, ValidatorConfig.Schema.password.min?.message)
+    .max(ValidatorConfig.Schema.password.max?.value!, ValidatorConfig.Schema.password.max?.message)
+    .regex(new RegExp(ValidatorConfig.Schema.password.regex?.value!), ValidatorConfig.Schema.password.regex?.message)
+
+export const zUsernameValidator = zRequiredValidator(ValidatorConfig.Schema.username.required?.message)
+    .min(ValidatorConfig.Schema.username.min?.value!, ValidatorConfig.Schema.username.min?.message)
+    .max(ValidatorConfig.Schema.username.max?.value!, ValidatorConfig.Schema.username.max?.message)
+    .regex(new RegExp(ValidatorConfig.Schema.username.regex?.value!), ValidatorConfig.Schema.username.regex?.message)
+```
---
+++ lib/types/index.d.ts
@@ -0,0 +1,102 @@
+export type Payload<M extends string> = {
+    required?: Value<undefined, M>;
+    email?: Value<string, M>;
+    min?: Value<number, M>;
+    max?: Value<number, M>;
+    regex?: Value<string | RegExp, M>;
+    domain?: Value<string | RegExp, M>;
+    url?: Value<string | RegExp, M>;
+    date?: Value<string | number | Date, M>;
+    extra?: Value<string, M>;
+    extra_1?: Value<string, M>;
+    extra_2?: Value<string, M>;
+};
+/**
+ * List message error keys, can be extended further.
+ * @param default_required Mark the target as required with default name is `__prefix__` + `default_required`, which will not allow undefined or null as a value
+ * @param required Mark the target as required with specific target name, which will not allow undefined or null as a value.
+ * @param required_min A minimum length limit for the target.
+ * @param required_max A maximum length limit for the target.
+ * @param length_invalid A required length for the specific target.
+ * @param domain_invalid A domain is invalid.
+ * @param phone_invalid A phone is invalid.
+ * @param date_invalid A date is invalid.
+ * @param url_invalid A URL is invalid.
+ * @param regex_invalid A regex is invalid.
+ * @param email_invalid A email is invalid.
+ * @param non_nullable The target is not nullable.
+ * @param non_undefined The target is not undefined.
+ */
+export type BaseMessage = 'default_required' | 'required' | 'required_min' | 'required_max' | 'length_invalid' | 'domain_invalid' | 'phone_invalid' | 'date_invalid' | 'url_invalid' | 'regex_invalid' | 'email_invalid' | 'non_nullable' | 'non_undefined';
+export type Value<T, M extends string> = {
+    message: M;
+    value?: T;
+};
+export type Define<T extends string, M extends string> = Record<T, Payload<M>>;
+/**
+ * The `flattenSchemaValueWith` function takes a schema object, a key, and an optional flag, and
+ * returns a flattened version of the schema values with modified keys.
+ * @param schema - The `schema` parameter is an object that represents a validation schema. It should
+ * have keys of type `K` and values of type `M`. The `Define` type is not defined in the code
+ * snippet, so I cannot provide more specific information about its structure.
+ * @param {K} key - The `key` parameter is a string that represents the key in the `schema` object that
+ * you want to flatten.
+ * @param [allowPrefixKey=true] - The `allowPrefixKey` parameter is a boolean value that determines
+ * whether to include the prefix key in the flattened key or not. If set to `true`, the flattened key
+ * will include the original key followed by a double underscore (`__`) and the message. If set to
+ * `false`, only the
+ * @returns The function `flattenSchemaValueWith` returns an object of type `Record<string, M>`, where
+ * `M` is the type of the values in the `target` object of the `schema`.
+ */
+export declare const flattenSchemaValueWith: <K extends string, M extends string>(schema: Define<K, M>, key: K, allowPrefixKey?: boolean) => Record<string, M> | null;
+/**
+ * The `flattenSchemaValue` function takes a schema object and flattens it into a new object with keys
+ * that combine the original keys and error messages.
+ * @param schema - The `schema` parameter is an object that represents a validation schema. It has keys
+ * of type `K` and values of type `M`.
+ * @param {string} [prefix] - The `prefix` parameter is an optional string that is used to add a prefix
+ * to the flattened keys. If a prefix is provided, it will be added to the beginning of each flattened
+ * key. If no prefix is provided, the flattened keys will not have any prefix.
+ * @returns The function `flattenSchemaValue` returns an object of type `Record<string, M>`, where `M`
+ * is the type of the values in the input `schema` object.
+ * @example
+ export namespace Validator {
+    export type Key = "username" | "email";
+    export const Schema: Define<Key, BaseMessage> = {
+      username: {
+        min: { message: "required_min", value: 8 },
+        max: { message: "required_max", value: 50 },
+        email: { message: "required_max", value: "50" },
+      },
+      email: {
+        min: { message: "required_min", value: 5 },
+        max: { message: "required_max", value: 255 },
+        email: { message: "email_invalid" },
+        regex: {
+          message: "regex_invalid",
+          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        },
+      },
+    };
+  }
+ flattenSchemaValue<Validator.Define.Key, BaseMessage>(Validator.Define.Schema)
+ */
+export declare const flattenSchemaValue: <K extends string, M extends string>(schema: Define<K, M>, prefix?: string) => Record<string, M>;
+/**
+ * The `flattenSchemaKey` function takes a key, message, and optional prefix, and returns a flattened
+ * key by concatenating them with underscores.
+ * @param {string} key - The `key` parameter is a string that represents a key in a schema. It is used
+ * to identify a specific field or property in the schema.
+ * @param {string} message - The `message` parameter is a string that represents the message associated
+ * with the schema key.
+ * @param {string} [prefix] - The `prefix` parameter is an optional parameter that represents a string
+ * that will be added as a prefix to the `flattenKey`. If the `prefix` parameter is not provided, the
+ * `flattenKey` will be the same as the `baseFlattenKey`.
+ * @returns the value of the variable `flattenKey`.
+ */
+export declare const flattenSchemaKey: (key: string, message: string, prefix?: string) => string;
+/**
+ * The function checks if a given message is equal to 'default_required'.
+ * @param {string} message - The `message` parameter is a string that represents a message.
+ */
+export declare const isDefaultRequiredMessage: (message: string) => boolean;
